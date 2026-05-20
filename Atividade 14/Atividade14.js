d123e6116559bfd5985797d5c6113a90bf3a06d0
I23Rodrigues <ir23rde@gmail.com>
2026-05-19 22:10:15 -0300

    Add files via upload

---
+++ Atividade 14/Atividade14.js
@@ -0,0 +1,13 @@
+function maiusculo() {
+
+    let texto = document.getElementById("texto");
+
+    texto.value = texto.value.toUpperCase();
+}
+
+function minusculo() {
+
+    let texto = document.getElementById("texto");
+
+    texto.value = texto.value.toLowerCase();
+}
